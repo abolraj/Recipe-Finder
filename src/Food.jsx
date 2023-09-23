d651a3d17de01348ac3f4e371290f3b71cca6a75
abolraj <142489829+abolraj@users.noreply.github.com>
2023-09-23 20:11:45 +0330

    feat: Add Food component & handle styles

---
+++ src/Food.jsx
@@ -0,0 +1,23 @@
+import { ArrowLeftIcon, ArrowRightCircleIcon, ArrowRightIcon } from '@heroicons/react/20/solid'
+
+export default function Food({data}){
+    return (
+        <div className="relative cursor-pointer group/food flex sm:flex-col h-32 sm:h-auto sm:w-52 lg:w-60 bg-opacity-20 bg-gray-600 rounded-3xl overflow-hidden text-left">
+            <img src={data.image} alt={data.name} className="w-full object-cover saturate-150 contrast-150 contrast-75 group-hover/food:contrast-100 transition-all"/>
+
+            <div className="p-6 box-border">
+                <h2 className="text-2xl group-hover/food:tracking-widest transition-all">
+                    {data.name}
+                </h2>
+                <p className="h-12 overflow-hidden text-clip">{data.description}</p>
+            </div>
+
+            <div className="invisible flex shadow-lg shadow-gray-800 justify-between items-center  group/details group-hover/food:visible h-8 hover:w-40 bg-gray-900 rounded-full p-2 px-4 absolute right-4 bottom-4 ml-20">
+                <p className="mb-1 hidden group-hover/details:block transition-all">See Details</p>
+                <ArrowRightCircleIcon className="h-full absolute right-0"/>
+            </div>
+        </div>
+        
+    )
+}
+
