bda7ff6a4c0bf40fef29eba8ea25105a60eace8f
abolraj <142489829+abolraj@users.noreply.github.com>
2023-09-24 20:39:37 +0330

    feat: Add API class to have an OOP & developer-friendly

---
+++ src/Api.js
@@ -0,0 +1,24 @@
+import $ from 'jquery'
+export default class RecipeApi {
+    static apiUrl = 'https://www.themealdb.com/api/json/v1/1/'
+    static search(done, data={}, fail=null){
+        this.request('search.php', done, data, fail)
+    }
+
+    static request(methodUrl, done, data = {}, fail = null){
+        $.ajax({
+            url: RecipeApi.apiUrl + methodUrl,
+            dataType: "json",
+            method: "GET",
+            data: data, 
+          })
+          .done(data=>{
+            done(data)
+            console.log('Success:', data)
+          })
+          .fail(err => {
+            fail && fail(err)
+            console.log('Error:', err)
+          })
+    }
+}
