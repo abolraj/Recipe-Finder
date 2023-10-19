6214ba35f7a4ea23d10e19a7dfbcea66d635d8bc
abolraj <142489829+abolraj@users.noreply.github.com>
2023-10-19 19:05:48 +0330

    feat: add another breakpoint on the tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
   content: [
     "./index.html",
@@ -6,6 +7,10 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
