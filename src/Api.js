91ff50ac103f9e6013e95f21f8e60c53c77f1c2c
abolraj <142489829+abolraj@users.noreply.github.com>
2023-10-19 11:38:15 +0330

    feat: handle find ajax request

--- src/Api.js
+++ src/Api.js
@@ -1,24 +1,28 @@
 import $ from 'jquery'
 export default class RecipeApi {
-    static apiUrl = 'https://www.themealdb.com/api/json/v1/1/'
-    static search(done, data={}, fail=null){
-        this.request('search.php', done, data, fail)
-    }
+  static apiUrl = 'https://www.themealdb.com/api/json/v1/1/'
+  static search(done, data = {}, fail = null) {
+    this.request('search.php', done, data, fail)
+  }
 
-    static request(methodUrl, done, data = {}, fail = null){
-        $.ajax({
-            url: RecipeApi.apiUrl + methodUrl,
-            dataType: "json",
-            method: "GET",
-            data: data, 
-          })
-          .done(data=>{
-            done(data)
-            console.log('Success:', data)
-          })
-          .fail(err => {
-            fail && fail(err)
-            console.log('Error:', err)
-          })
-    }
+  static find(done, data = {}, fail = null) {
+    this.request('lookup.php', done, data, fail)
+  }
+
+  static request(methodUrl, done, data = {}, fail = null) {
+    $.ajax({
+      url: RecipeApi.apiUrl + methodUrl,
+      dataType: "json",
+      method: "GET",
+      data: data,
+    })
+    .done(data => {
+      done(data)
+      console.log('Success:', data)
+    })
+    .fail(err => {
+      fail && fail(err)
+      console.log('Error:', err)
+    })
+  }
 }
